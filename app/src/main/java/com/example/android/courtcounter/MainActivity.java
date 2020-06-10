75b9d68d36e2fa57ce7e2df49e625702aa4d2a03
IbsanjU <bharath2668@gmail.com>
2020-06-10 16:00:43 +0530

    Buttons Onclicks

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -3,14 +3,47 @@ package com.example.android.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
 
 public
 class MainActivity extends AppCompatActivity {
+    int scoreTeamA =0;
+
 
     @Override
     protected
     void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayForTeamA(0);
     }
+
+    public
+    void displayForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public
+    void addThreeForTeamA(View v) {
+        scoreTeamA=scoreTeamA+3;
+        displayForTeamA(scoreTeamA);
+    }
+    public
+    void addTwoForTeamA(View v) {
+        scoreTeamA=scoreTeamA+2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public
+
+    void addOneForTeamA(View v) {
+        scoreTeamA=scoreTeamA+1;
+        displayForTeamA(scoreTeamA);
+    }
+
 }
